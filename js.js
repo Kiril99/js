676528aca2fad2255d11af59dc105a72aed9b2ae
Kiril <kpanar82@gmail.com>
2018-03-23 12:49:32 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,116 @@
+// function bon (){
+// 	var sum = Number(prompt("Введите число"));
+// 	var a = 1;
+// 	var b = 1;
+// 	var = c;
+// 	for (var i = 3; i<= sum; i++) {
+// 			c = a + b;
+// 			a = b;
+// 			b = c;
+// 	}
+// 	alert(b);
+// }
+
+// bon();
+
+// var count = 0;
+// for (var i =2; i<100; i++){
+// 	for(var j=1;i<i;i++){
+// 		if (i%j===0) {
+// 			count++;
+// 		}
+// 	}
+// 	if(count<2){
+// 		console.log(i);
+// 	}
+// 	count=0;
+
+// }
+
+// var count = 0;
+// for (var i =2; i<100; i++){
+// 	for(var j=1;i<i;i++){
+// 		if (i%j===0) {
+// 			count++;
+// 		}
+// 	}
+// 	if(count<2){
+// 		count.push(i);
+// 	}
+// 	count=0;
+
+// }
+
+
+// var a = [1,2,3,4,5,6];
+// var b = [7,8,9,10,11,12];
+// var c = [13,14,15,16,17,18];
+// var d = [19,20,21,22,23,24];
+
+// var matr = [];
+
+// matr.push(a,b,c,d);
+
+// for(i=0;i<matr.length;i++){
+// 	for (var j =0; j<matr[i].length;i++){
+// 		console.log(matr[i][j]);
+// 	}
+
+// }
+
+
+// function arrFibb(){
+// 	var num = Number(prompt("input number"))
+// 	var arr=[];
+// 	var a = 1;
+// 	var b = 1;
+// 	for (var i=1; i<=num;i++){
+// 		var c = a + b;
+// 		a=b;
+// 		b=c;
+// 		arr.push(a);
+// 	}
+// 	alert(arr);
+// }	
+
+// arrFibb();
+
+// var matr = [
+// ["r",4,"ar",true,8,"bn"],
+// ["r",7,"ar",true,8,"bn"],
+// ["r",true,"ar",true,8,"bn"],
+// ["r","ar","ar",1,8,"bn"],
+// ["r",false,"ar",true,8,"bn"],
+// ["r",10,"ar",true,8,"bn"]
+// ];
+// var arr[];
+// for(i=;i<matr.length;i++){
+// 	for (var j =0; j<matr[i].length; i++) {
+// 		if (typeof(matr[i][j])=="number") {
+// 			arr.push(matr[i][j]);
+// 		}
+		
+// 	}
+
+// }
+// alert(arr)
+
+
+// var matr = [
+// [1,5,7],
+// [1,5,7],
+// [1,5,7],
+
+// ];
+
+// function diagonal(mass){
+// 	var arr=[];
+// 	var pos = 1;
+// 	for (var i = 0; i<mass.length;i++) {
+// 		arr.push(mass[i][mass.length-pos]);
+// 		pos++;
+// 	}
+// 	alert(arr);
+
+// }
+// diagonal(matr);
